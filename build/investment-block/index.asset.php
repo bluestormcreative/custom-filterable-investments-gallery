e497ad50f03cffbfaa6340be921d31fc3316c67d
Max MacMillan <max@treat.xyz>
2023-08-21 11:22:22 -0400

    build

--- build/investment-block/index.asset.php
+++ build/investment-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '236c9435a272bc3e8fdb');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '324e53398a36f67dcf0a');
