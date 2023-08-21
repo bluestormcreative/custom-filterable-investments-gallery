6b3778a949c63effa3d045f5043c204c7f18584b
Max MacMillan <max@treat.xyz>
2023-08-21 11:51:42 -0400

    Set additional atts for logo

--- src/investment-block/edit.js
+++ src/investment-block/edit.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, MediaPlaceholder } from '@wordpress/block-editor';
 import { useSelect } from "@wordpress/data";
@@ -22,11 +23,15 @@ export default function Edit({ attributes, setAttributes }) {
 			return {
 				imageUrl: url,
 				imageId: image?.id,
-				imageAlt: image?.alt_text, 
+				imageAlt: image?.alt_text || __('investment logo', 'cfig'), 
 			};
 		},
 		[logoId]
 	);
+
+	useEffect(() => {
+		setAttributes({ logoUrl: logo.imageUrl, logoAlt: logo.imageAlt });
+	}, [logo]);
 
 	const handleRemoveImage = () => {
 		setAttributes({ logoId: undefined, logoUrl: undefined, logoAlt: undefined });
