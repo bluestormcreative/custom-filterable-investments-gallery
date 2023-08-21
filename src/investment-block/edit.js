5ee2af36d74c07c2062a9bdd8dc9d6b324900522
Max MacMillan <max@treat.xyz>
2023-08-21 00:01:41 -0400

    Add the logo media controls and handlers

--- src/investment-block/edit.js
+++ src/investment-block/edit.js
@@ -1,14 +1,62 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
-import { TextControl } from "@wordpress/components";
+import { useBlockProps, MediaPlaceholder } from '@wordpress/block-editor';
+import { useSelect } from "@wordpress/data";
+import { TextControl, Button } from "@wordpress/components";
 import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
-	const blockProps = useBlockProps({ className: "investment-block editor" });
-	const { business, sector, years, type } = attributes;
+	const blockProps = useBlockProps({ className: "investment-block" });
+	const {logoId, business, sector, years, type } = attributes;
+
+	const logo = useSelect(
+		(select) => {
+			const image = logoId && select("core").getMedia(logoId);
+			return {
+				imageUrl: image?.media_details?.sizes?.investment_logo?.source_url,
+				imageId: logoId ? select("core").getMedia(logoId) : null,
+			}
+		},
+		[logoId]
+	);
+
+	const handleRemoveImage = () => {
+		setAttributes({ logoId: null });
+	}
+
+	const renderLogoPreview = (logo) => (
+		<div className="block-editor-logo-preview">
+			<span>Logo</span>
+			<div className="logo-content">
+				<img src={logo.imageUrl} />
+				<Button variant="secondary" onClick={handleRemoveImage}>
+					Remove
+				</Button>
+			</div>
+		</div>
+	);
 
 	return (
 		<div {...blockProps}>
+			{logo && logo.imageUrl ? (
+				renderLogoPreview(logo)
+			) : (
+				<MediaPlaceholder
+					labels={{
+						title: __("Logo", "cfig"),
+						instructions: __(
+							"Upload a logo or select one from the media library.",
+							"cfig"
+						),
+					}}
+					onSelect={(image) => setAttributes({ logoId: image.id })}
+					accept="image/*"
+					allowedTypes={["image"]}
+					multiple={false}
+					value={{
+						id: attributes.logoId,
+					}}
+				/>
+			)}
 			<TextControl
 				label={__("Business", "cfig")}
 				value={business}
