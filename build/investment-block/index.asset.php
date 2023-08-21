b252ad6994f5208d92dc31e76e9edc79c5abc506
Max MacMillan <max@treat.xyz>
2023-08-21 11:03:03 -0400

    Remove picker dep, build

--- build/investment-block/index.asset.php
+++ build/investment-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '7d675ae88bbeb2e5e7e9');
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '236c9435a272bc3e8fdb');
